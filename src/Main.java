5a79cec3d42d8a0ba50cc0a7b3e39f8ba1b3e792
Zachnp12 <98759160+Zachnp12@users.noreply.github.com>
2022-02-06 23:24:24 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int springSeason = 105;
+        int summerSeason = 50;
+        int fallSeason = 30;
+        int winterSeason = 40;
+	System.out.println("What is the Spring season cost? ");
+    System.out.println("What is the Summer season cost? ");
+    System.out.println("What is the Fall season cost? ");
+    System.out.println("What is the Winter season cost? ");
+    int totalCost = springSeason + summerSeason + fallSeason + winterSeason;
+    System.out.println("The total cost is: " + totalCost);
+    }
+}
